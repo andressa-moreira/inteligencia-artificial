a6e79314c341d5f9fa77afd609c747904a73bcd7
andressa-moreira <106606769+andressa-moreira@users.noreply.github.com>
2024-07-04 19:22:26 -0300

    ok

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
+
 const perguntas = [
     {
       enunciado:
@@ -11,11 +12,11 @@ const perguntas = [
       alternativas: [
         {
           texto: "Isso é assustador!",
-          afirmacao: "afirmação",
+          afirmacao: "No início ficou com medo do que essa tecnologia pode fazer."
         },
         {
           texto: "Isso é maravilhoso!",
-          afirmacao: "afirmação",
+          afirmacao: "Quis saber como usar IA no seu dia a dia."
         },
       ],
     },
@@ -87,49 +88,54 @@ const perguntas = [
 
 let atual = 0;
 let perguntaAtual;
-let historiaFinal = "";
+let historiaFinal ="";
 
 function mostraPergunta() {
-    if (atual >= perguntas.length) {
-        mostraResultado();
-        return;
-    }
-    perguntaAtual = perguntas[atual];
-    caixaPerguntas.textContent = perguntaAtual.enunciado;
-     caixaAlternativas.textContent = "";
-    mostraAlternativas();
+  if (atual >= perguntas.length) {
+      mostraResultado();
+      return;
+  }
+  perguntaAtual = perguntas[atual];
+  caixaPerguntas.textContent = perguntaAtual.enunciado;
+   caixaAlternativas.textContent = "";
+  mostraAlternativas();
 }
+
 
 function respostaSelecionada(opcaoSelecionada) {
-    const afirmacoes = opcaoSelecionada.afirmacao;
-    historia += afirmacoes + "";
-    atual++;
-    mostraPergunta();
+  const afirmacoes = opcaoSelecionada.afirmacao;
+  historia += afirmacoes + "";
+  atual++;
+  mostraPergunta();
 }
+
+
 
 function mostraResultado() {
-    caixaPerguntas.textContent = "Em 2049...";
-    textoResultado.textContent = historiaFinal;
-    caixaAlternativas.textContent = "";
+  caixaPerguntas.textContent = "Em 2049...";
+  textoResultado.textContent = historiaFinal;
+  caixaAlternativas.textContent = "";
 }
 
-function mostraAlternativas() {
-    for (const alternativa of perguntaAtual.alternativas) {
-      const botaoAlternativas = document.createElement("button");
-      botaoAlternativas.textContent = alternativa.texto;
-      botaoAlternativas.addEventListener("click", function () {
-        atual++;
-        mostraPergunta();
-      });
-      caixaAlternativas.appendChild(botaoAlternativas);
-    }
-  }
 
-  function respostaSelecionada(opcaoSelecionada){
-    const afirmacoes = opcaoSelecionada.afirmacoes;
-    historiaFinal = afirmacoes;
-    atual++;
-    mostraPergunta();
+
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas){
+            const botaoAlternativas = document.createElement("button");
+            botaoAlternativas.textContent = alternativa.texto;
+            botaoAlternativas.addEventListener("click", function(){
+                atual++;
+                mostraPergunta();
+            })
+            caixaAlternativas.appendChild(botaoAlternativas);
+    }
+
+}
+function respostaSelecionada(opcaoSelecionada){
+  const afirmacoes = opcaoSelecionada.afirmacoes;
+  historiaFinal = afirmacoes;
+  atual++;
+  mostraPergunta();
 }
 mostraPergunta();
 
